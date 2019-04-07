5b85c32844c983a815ebf8a158f0af6bc42dac38
jonDatz <notjondatz@gmail.com>
2019-04-07 09:58:38 -0400

    test build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper/Wrapper";
 import Navbar from "./components/Navbar/Navbar";
 import CharacterCard from "./components/CharacterCard/CharacterCard";
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
